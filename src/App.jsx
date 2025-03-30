eb83fa3dccfd9c53f2915aad3d6a90f224d0172a
VishwasMB10 <vsmb.2410@gmailcom>
2025-03-31 00:43:40 +0530

    Complete basic data fetch from db

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import style from '../stylesheets/App.module.css';
 import Form from '../components/Form';
-
+import {supabase} from './supabaseClient';
+import Card from '../components/Card';
 
 function App() {
 
   const [isClicked,setIsClicked]=useState(false);
   const [cards,setCards]=useState([]);
+  const [data,setData]=useState([]);
 
   function formDisplay(){
     setIsClicked((isClicked)=>!isClicked);
   }
+
+  useEffect(()=>{
+    async function fetchDetails(){
+      const {data,error}=await supabase.from('debit').select('*');
+      if(error){
+        console.log("Error fetching the details: ",error);
+      }
+      else{
+        setData(data);
+        console.log("here");
+      }
+    }
+    fetchDetails();
+  },[]);
+
+  useEffect(()=>{
+    setCards((prev)=>{return data.map((ele)=>{return <Card key={ele.product} product={ele.product} quantity={ele.quantity} amount={ele.amount} />});});
+  },[data]);
 
 
   return (
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://uvwxchyurzxfgeglahcu.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2d3hjaHl1cnp4ZmdlZ2xhaGN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMyNjg2NTQsImV4cCI6MjA1ODg0NDY1NH0.iOP6Hh9OOzIN2t5-cEAMH5SyRdSPNvMNU1KCkrakWDI"
 const supabase = createClient(supabaseUrl, supabaseKey);
+export {supabase}; 
